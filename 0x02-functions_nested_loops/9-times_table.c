6efb77076decf435eec544843dfedb033ca4087d
QingMarcel <88100657+QingMarcel@users.noreply.github.com>
2022-03-16 20:37:51 +0100

    Update 9-times_table.c
    This is my changes Marcel

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,23 +9,23 @@ void times_table(void)
 {
 	int r, c, d;
 
-	for (r = 0; r <= 9; r++)
+        for (r = 0; r <= 9; r++)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		f
-		else
-		{
-			_putchar(' ');
-		}
-			_putchar((d %or (c = 1; c <= 9; c++)
+		for (c = 1; c <= 9; c++)
 		{
 			d = (r * c);
 		if ((d / 10) > 0)
 		{
 			_putchar((d / 10) + '0');
-		} 10) + '0');
+		}
+		else
+		{
+			_putchar(' ');
+		}
+			_putchar((d % 10) + '0');
 		if (c < 9)
 		{
 			_putchar(',');
