96287a1257d93a2caea15f3575469f0404021880
Agathaonalo <agathaonalo15@gmail.com>
2022-03-16 16:47:20 +0100

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar ending with a neww line
+ *
+ * Description: Prints _putchar
+ *
+ * Return: o (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
